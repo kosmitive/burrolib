fixed valuee function computation

burro/agents/categorical_agent.py
@@ -23,7 +23,8 @@ class CategoricalAgent(Agent):
         self.vf_epochs = vf_epochs
 
     def clone(self):
-        return CategoricalAgent(deepcopy(self.policy_model), batch_size=self.batch_size)
+        return CategoricalAgent(deepcopy(self.policy_model), gamma=self.gamma, batch_size=self.batch_size,
+                                vf_epochs=self.vf_epochs)
 
     def get_outgoing_orders(self, pos, clen, stock, iorders, oorders, costs):
         state = torch.stack([torch.tensor(stock, dtype=torch.float32), torch.tensor(iorders, dtype=torch.float32),
@@ -38,7 +39,7 @@ class CategoricalAgent(Agent):
             discounted_reward = torch.cat([torch.tensor(dr) for dr in discounted_reward])
             self.batch_fill_count = 0
             self.update_vf(self.states_batch, discounted_reward)
-            advantage = self.policy_model.vf_net(self.states_batch) - discounted_reward
+            advantage = discounted_reward - self.policy_model.vf_net(self.states_batch)
             self.update_policy(self.states_batch, advantage, self.oorders_batch)
 
         return new_oorders

burro/policies/categorical_policy.py
@@ -28,8 +28,8 @@ class CategoricalPolicyModel(BasePolicyModel):
                  vf_hidden_size: Tuple[int] = (64, 64,), p_lr: float = 1e-3, vf_lr: float = 1e-3):
         super(CategoricalPolicyModel, self).__init__()
         self.max_order_size = max_order_size
-        self.policy_net = MLP(in_dim=io_order_dim, out_dim=max_order_size, hidden_size=p_hidden_size, nonlin='relu')
-        self.vf_net = MLP(in_dim=io_order_dim, out_dim=1, hidden_size=vf_hidden_size, nonlin='relu')
+        self.policy_net = MLP(in_dim=io_order_dim, out_dim=max_order_size, hidden_size=p_hidden_size, nonlin='elu')
+        self.vf_net = MLP(in_dim=io_order_dim, out_dim=1, hidden_size=vf_hidden_size, nonlin='elu')
         self.policy_optim = torch.optim.Adam(self.policy_net.parameters(), lr=p_lr)
         self.vf_optim = torch.optim.Adam(self.vf_net.parameters(), lr=vf_lr)
 

burro/sim/simulator.py
@@ -84,9 +84,9 @@ class Simulator:
         self.current_supply += self.running_deliveries[:-1]
 
         new_orders = self.process.get_discrete_increase()
-        print("Supply: ", self.current_supply)
-        print("Orders: ", self.orders, " -> ", new_orders)
-        print("Cost: ", self.costs)
+        # print("Supply: ", self.current_supply)
+        # print("Orders: ", self.orders, " -> ", new_orders)
+        # print("Cost: ", self.costs)
 
         # add cost
         self.generated_delay_costs = self.orders[1:] * 1.0
@@ -94,7 +94,7 @@ class Simulator:
 
         # place the end consumer order
         self.orders[-1] += new_orders
-        print("End Order: ", self.costs)
+        # print("End Order: ", self.costs)
         self.orders[:-1] += [self.agents[k].get_outgoing_orders(k, self.N, self.current_supply[k],
                                                                 self.orders[k + 1], self.orders[k], self.generated_costs[k])
                              for k in range(self.N)]
@@ -112,7 +112,7 @@ class Simulator:
             self.plot_frame()
         return np.sum(self.generated_costs)
 
-    def mult_steps(self, steps):
+    def mult_steps(self, steps: int) -> np.ndarray:
         """Performs multiple steps using the one step method."""
 
         res = np.asarray([self.one_step() for _ in range(steps)])